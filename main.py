81a02698df774313e5b87ff00a11a295e962bf70
alexismarroquin7 <alexismarroquin754@gmail.com>
2022-11-30 22:49:44 -0800

    TransactionTrackerCli scaffolding

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.TransactionTracker import TransactionTracker;
+
+if __name__ == "__main__":
+  trxTracker = TransactionTracker();
+  trxTracker.run();
---
+++ src/TransactionTracker.py
@@ -0,0 +1,232 @@
+import os;
+import json;
+import datetime;
+
+def menu_input (**config):
+  
+  prompt = config["prompt"] if "prompt" in config else "";
+  options = config["options"] if "options" in config else [];
+  error_message = config["error_message"] if "error_message" in config else "\nInvalid input\n";
+  initial_response = config["initial_response"] if "initial_response" in config else None;
+  
+  if (prompt == ""):
+    for opt in options:
+      prompt += f'{opt["in"]}) {opt["out"]}\n';
+    
+    prompt += "\n";
+
+  response = initial_response;
+  running = True;
+  while(running):
+    if(response != initial_response):
+      print(error_message);
+    
+    response = input(prompt)
+
+    for opt in options:
+      if (response == opt["in"]):
+        response = opt["out"];
+        print("\n");
+        running = False;
+        break;
+
+  return response;
+  
+
+class TransactionTracker () :
+  def __init__(self, **config):
+    file_name = config["file_name"] if "file_name" in config else "data";
+    self.file_path = f"{file_name}.json";
+
+  def setup (self):
+    is_file = os.path.isfile(self.file_path);
+
+    if(not is_file):
+      f = open(self.file_path, "w");
+      
+      data = {
+        "transactions": []
+      };
+
+      json.dump(data, f, indent = 2);
+
+      f.close();
+
+  def get_transactions (self):
+    f = open (self.file_path, "r");
+
+    data = json.loads(f.read());
+    
+    f.close();
+
+    return data["transactions"];
+
+  def set_transactions (self, trx_list):
+    
+    f = open (self.file_path, "r");
+    data = json.loads(f.read());
+    f.close();
+
+    f = open(self.file_path, "w");
+    
+    newData = {
+      **data,
+      "transactions": trx_list
+    }
+
+    json.dump(newData, f, indent = 2);
+    
+    f.close();
+
+    return self.get_transactions();
+
+  def get_transaction_by_index (self, index):
+    trx_list = self.get_transactions();
+    return trx_list[index];
+
+  def create_deposit (self):
+    print("Creating deposit...");
+    name = input("Enter name: ");
+    description = input("Enter description: ");
+    amount = input("Enter amount: ");
+    timestamp = datetime.datetime.now();
+
+    trx_list = self.get_transactions();
+
+    id = None;
+    
+    if(len(trx_list) == 0):
+      id = 1;
+    else:
+      id = trx_list[-1]["id"] + 1;
+
+  
+    trx_list.append({
+      "id": id,
+      "name": name,
+      "description": description,
+      "amount": float(amount),
+      "timestamp": str(timestamp),
+      "type": "deposit"
+    });
+
+    self.set_transactions(trx_list);
+
+  def create_withdrawal (self):
+    print("Creating withdrawal...");
+    name = input("Enter name: ");
+    description = input("Enter description: ");
+    amount = input("Enter amount: ");
+    timestamp = datetime.datetime.now();
+
+    trx_list = self.get_transactions();
+    
+    id = None;
+    
+    if(len(trx_list) == 0):
+      id = 1;
+    else:
+      id = trx_list[-1]["id"] + 1;
+    
+    trx_list.append({
+      "id": id,
+      "name": name,
+      "description": description,
+      "amount": float(amount),
+      "timestamp": str(timestamp),
+      "type": "withdrawal"
+    });
+
+    self.set_transactions(trx_list);
+
+  def create (self):
+    print("What type of transaction do you want to make?");
+    trx_type = menu_input(
+      options = [
+        {
+          "in": "1",
+          "out": "deposit"
+        },
+        {
+          "in": "2",
+          "out": "withdrawal"
+        },
+      ]
+    );
+
+    if(trx_type == "deposit"):
+      self.create_deposit();
+    elif(trx_type == "withdrawal"):
+      self.create_withdrawal();
+  
+  def view_all (self):
+    trx_list = self.get_transactions();
+
+    print('id | timestamp | type | amount | name | description');
+
+    balance = 0;
+    deposit_count = 0;
+    withdrawal_count = 0;
+
+    for trx in trx_list:
+      s = f'{trx["id"]} | {trx["timestamp"]} | {trx["type"]} | {trx["amount"]} | {trx["name"]} | {trx["description"]}';
+      print(s);
+
+      if(trx["type"] == "deposit"):
+        balance += trx["amount"];
+        deposit_count += 1;
+      elif(trx["type"] == "withdrawal"):
+        balance += (trx["amount"] * -1);
+        withdrawal_count += 1;
+    
+    print('balance | deposit count | withdrawal count');
+    print(f'{balance} | {deposit_count} | {withdrawal_count}');
+
+  def view (self):
+    print('Select view option: ');
+    menu_option = menu_input(
+      options = [
+        {
+          "in": "1",
+          "out": "all"
+        }
+      ]
+    )
+
+    if(menu_option == "all"):
+      self.view_all();
+    
+
+
+  def run (self):
+    
+    self.setup();
+
+    print('Transaction Tracker CLI');
+
+    menu_option = menu_input(
+      options = [
+        {
+          "in": "1",
+          "out": "create"
+        },
+        {
+          "in": "2",
+          "out": "view"
+        },
+        {
+          "in": "3",
+          "out": "update"
+        },
+        {
+          "in": "4",
+          "out": "delete"
+        },
+      ]
+    );
+
+    if(menu_option == "create"):
+      self.create();
+    elif(menu_option == "view"):
+      self.view();
+
